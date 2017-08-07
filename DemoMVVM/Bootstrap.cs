018332e889e20cf3ea684a9fdc59afc39c784cbe
Satsui Bird <satsuibird@messatsu-dojo.com>
2017-08-08 01:54:52 +0200

    Main solution created

---
+++ DemoMVVM/Bootstrap.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Diagnostics;
+
+namespace DemoMVVM
+{
+    public partial class Bootstrap
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            Debug.WriteLine("OnStartup()");
+        }
+
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Debug.WriteLine("OnExist()");
+
+            base.OnExit(e);
+        }
+
+    }
+}
